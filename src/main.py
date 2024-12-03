41e45118ae9cbb216654dde5e164e11c9ae54b8c
ViKolXXXX <vitek323pvi@rambler.ru>
2024-12-03 10:43:34 +0300

    ADD src

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-import sys
-sys.path.append("..")
-from config.global_config import *
+#import sys
+#sys.path.append("..")
+#from config.global_config import *
 
 
-print(API_KEY_POGODA_OpenWeather)
-print(API_KEY_POGODA_OpenWeather)
-print(API_KEY_POGODA_OpenWeather)
+#print(API_KEY_POGODA_OpenWeather)
+#print(API_KEY_POGODA_OpenWeather)
+#print(API_KEY_POGODA_OpenWeather)
 
 print('Привет Мир')
