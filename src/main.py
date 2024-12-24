ea1e0479cb3402c74c4d83ad3f73044d7ff515b6
ViKolXXXX <vitek323pvi@rambler.ru>
2024-12-24 18:51:50 +0300

    ADD /.idea/

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import sys
+from os.path import join, dirname
+import os
 sys.path.append("..")
 from config.global_config import *
 
 
-print(API_KEY_POGODA_OpenWeather)
-print(API_KEY_POGODA_OpenWeather)
-print(API_KEY_POGODA_OpenWeather)
+print(API_KEY_BOT)
