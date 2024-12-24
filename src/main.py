a3c14f17b19e50da53bc67b55c3dc2ee696a0061
ViKolXXXX <vitek323pvi@rambler.ru>
2024-12-24 18:54:41 +0300

    test

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import sys
 from os.path import join, dirname
 import os
@@ -7,15 +7,5 @@ from config.global_config import *
 
 
 print(API_KEY_BOT)
-=======
-#import sys
-#sys.path.append("..")
-#from config.global_config import *
 
 
-#print(API_KEY_POGODA_OpenWeather)
-#print(API_KEY_POGODA_OpenWeather)
-#print(API_KEY_POGODA_OpenWeather)
-
-print('Привет Мир')
->>>>>>> telegram
